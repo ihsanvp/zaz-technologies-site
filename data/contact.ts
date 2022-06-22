0284471ee7db66195e7c2c25124a9f1b6dde9b7f
ihsanvp <ihsanvp2738@gmail.com>
2022-06-22 19:07:22 +0530

    update number

--- data/contact.ts
+++ data/contact.ts
@@ -3,8 +3,8 @@ const contactData = {
     `Corporate Office: Suite 804, Millenium Plaza Hotel, Sheikh Zayed Road.`,
     `Branch Office: 709, Al Qusais Plaza, Damascus Street, Dubai`,
   ],
-  mobiles: ["+971 55 140 5631"],
-  mails: ["Email-support@zazplus.com"],
+  mobiles: ["054 4555750"],
+  mails: ["support@zazplus.com"],
 };
 
 export default contactData;
---
+++ public/api/message.php
@@ -0,0 +1,53 @@
+<?php
+
+function validate()
+{
+    if (array_key_exists("name", $_POST) && array_key_exists("email", $_POST) && array_key_exists("phone", $_POST) && array_key_exists("message", $_POST)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    if (validate()) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $message = $_POST["message"];
+        $message = wordwrap($message, 70);
+
+        $to = 'shameer@zazplus.com';
+        $subject = "Message from {$name}";
+
+        $msg = "
+        Name : {$name}\n
+        Email : {$email}\n
+        Phone no : {$phone}\n
+        \n
+        {$message}
+        ";
+
+        $success =  mail($to, $subject, $msg);
+
+        if (!$success) {
+            http_response_code(500);
+            exit();
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode([
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            'message' => $message,
+            'success' => $success
+        ]);
+    } else {
+        http_response_code(400);
+    }
+} else {
+    http_response_code(405);
+}
